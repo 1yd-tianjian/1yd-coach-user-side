9cdd4b9cd5d87eeb2307eebe6f31c4e29167f954
yeting <yeting@tianjiancloud.com>
2015-04-22 13:10:44 +0800

    Service 服务集成

--- src/app/coach/coach.controller.js
+++ src/app/coach/coach.controller.js
@@ -2,160 +2,178 @@
 
 angular.module('1yd-coach')
 
-.controller('CoachListCtrl', function($scope, coachService, meatDataService) {
+  .controller('CoachListCtrl', function ($scope, coachService, meatDataService) {
 
 
-  meatDataService.meatData().then(function(data){
-    console.log(data);
-    //运动类型
-    $scope.categories = data.categories;
-    //排序方式
-    $scope.sorts = data.sorts;
-    //行政区
-    $scope.districts = data.city_districts[0].districts;
-    //商圈
-    $scope.cbds = data.city_districts[0].districts[0].district_cbds[0];
-    console.log($scope.cbds);
-  });
+    meatDataService.meatData().then(function (data) {
+      console.log(data);
+      //运动类型
+      $scope.categories = data.categories;
+      //排序方式
+      $scope.sorts = data.sorts;
+      //行政区
+      $scope.districts = data.city_districts[0].districts;
+      //商圈
+      $scope.cbds = data.city_districts[0].districts[0].district_cbds[0];
+      console.log($scope.cbds);
+    });
 
-  //页面初始化
-  var vm = $scope.vm = {
-    mask: false,
-    pageInfo: {}
-  };
+    //页面初始化
+    var vm = $scope.vm = {
+      mask: false,
+      pageInfo: {}
+    };
 
-  //筛选条切换
-  $scope.currentFilter = null;
-  $scope.setFilter = function(filter) {
-    $scope.currentFilter = filter;
-    vm.mask = true;
-  };
-
-  //隐藏遮罩层
-  function hideMask() {
-    vm.mask = false;
+    //筛选条切换
     $scope.currentFilter = null;
-  };
-  $scope.hideMask = function() {
-    hideMask();
-  };
+    $scope.setFilter = function (filter) {
+      $scope.currentFilter = filter;
+      vm.mask = true;
+    };
 
-  // 收藏教练
-  $scope.collect = function (){
-    alert('sfds');
-    return;
-  };
+    //隐藏遮罩层
+    function hideMask() {
+      vm.mask = false;
+      $scope.currentFilter = null;
+    };
+    $scope.hideMask = function () {
+      hideMask();
+    };
 
-  //初始化教练列表
-  $scope.coaches = [];
+    // 收藏教练
+    $scope.collect = function () {
+      alert('sfds');
+      return;
+    };
 
-  var param = {
-    size: 10,
-    page: 0,
-    category_id: null,
-    gender: null
-  };
+    //初始化教练列表
+    $scope.coaches = [];
 
-  /**
-   * 查询教练列表
-   * @param  {[obj]} param [page,size,category_id,gender]
-   * @return {[arr]}       [coachList]
-   */
-  coachService.allCoaches(param).then(function(res) {
-    vm.pageInfo = _.assign(vm.pageInfo, res.info);
-    $scope.coaches = res;
-  }, function(err) {
-    console.log(err);
-  });
+    var param = {
+      size: 10,
+      page: 0,
+      category_id: null,
+      gender: null
+    };
 
-  // 刷新列表
-  $scope.doRefresh = function() {
-    param.page = 0;
-    coachService.allCoaches(param).then(function(res) {
+    /**
+     * 查询教练列表
+     * @param  {[obj]} param [page,size,category_id,gender]
+     * @return {[arr]}       [coachList]
+     */
+    coachService.allCoaches(param).then(function (res) {
+      vm.pageInfo = _.assign(vm.pageInfo, res.info);
+      $scope.coaches = res;
+    }, function (err) {
+      console.log(err);
+    });
+
+    // 刷新列表
+    $scope.doRefresh = function () {
+      param.page = 0;
+      coachService.allCoaches(param).then(function (res) {
         vm.pageInfo = _.assign(vm.pageInfo, res.info);
         $scope.coaches = res;
       })
-      .finally(function() {
-        $scope.$broadcast('scroll.refreshComplete'); // Stop the ion-refresher from spinning
-      });
-  };
-
-  // 加载更多
-  $scope.loadMore = function() {
-    param.page += 1;
-    if (!vm.pageInfo.last) {
-      coachService.allCoaches(param).then(function(res) {
-        vm.pageInfo = _.assign(vm.pageInfo, res.info);
-        $scope.coaches = $scope.coaches.concat(res);
-        $scope.$broadcast('scroll.infiniteScrollComplete'); // Stop the ion-refresher from spinning
-      });
+        .finally(function () {
+          $scope.$broadcast('scroll.refreshComplete'); // Stop the ion-refresher from spinning
+        });
     };
-  };
 
-  // 页面回收机制
-  $scope.$on('$stateChangeSuccess', function() {
-    hideMask();
+    // 加载更多
+    $scope.loadMore = function () {
+      param.page += 1;
+      if (!vm.pageInfo.last) {
+        coachService.allCoaches(param).then(function (res) {
+          vm.pageInfo = _.assign(vm.pageInfo, res.info);
+          $scope.coaches = $scope.coaches.concat(res);
+          $scope.$broadcast('scroll.infiniteScrollComplete'); // Stop the ion-refresher from spinning
+        });
+      }
+      ;
+    };
+
+    // 页面回收机制
+    $scope.$on('$stateChangeSuccess', function () {
+      hideMask();
+    });
+
+  })
+
+
+  .controller('CoachDetailCtrl', function ($scope, $stateParams, $ionicModal, Restangular, coachService, CoachSvc) {
+    var coachId = $stateParams.coachId;
+
+    //获取单个教练数据
+
+    //var baseCoaches = Restangular.one('coaches', coachId);
+    /**
+     * 获取教练信息
+     */
+    var coachPromise = coachService.find(coachId);
+    coachPromise.then(function (data) {
+      $scope.coach = data;
+      data.courses().then(function (courses) {
+        $scope.courses = courses;
+      });
+    });
+    //var coursePromise = coachPromise.call('test');
+    //coursePromise.then(function(data) {
+    //
+    //console.log('coursePromise',data);
+    //})
+    //coursePromise.then(function(data) {
+    //  console.log('coursePromise',data);
+    //});
+    //$scope.courses = baseCoaches.getList('courses').$object;
+    // $scope.comments = baseCoaches.getList('comments').$object;
+
+
+    //初始化推荐教练列表
+    $scope.reCoaches = [];
+
+    var param = {
+      size: 4,
+      page: 0,
+      category_id: null,
+      gender: null
+    };
+
+    /**
+     * 查询推荐教练列表
+     * @param  {[obj]} param [page,size,category_id,gender]
+     * @return {[arr]}       [coachList]
+     */
+    coachService.allCoaches(param).then(function (res) {
+      //console.log(res);
+      $scope.reCoaches = res;
+    }, function (err) {
+      console.log(err);
+    });
+
+    //课程安排modal
+    $ionicModal.fromTemplateUrl('app/components/modals/timetable/timetable.html', {
+      scope: $scope,
+      animation: 'slide-in-up',
+      hardwareBackButtonClose: true
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
+
+    //弹出课程安排modal
+    $scope.openSchedule = function () {
+      $scope.modal.show();
+    };
+
+    //关闭课程安排modal
+    $scope.closeSchedule = function () {
+      $scope.modal.hide();
+    };
+
+    //教练Tab
+    $scope.currentTab = "tab1";
+    $scope.setTab = function (tab) {
+      $scope.currentTab = tab;
+    };
+
   });
-
-})
-
-
-
-.controller('CoachDetailCtrl', function($scope, $stateParams, $ionicModal, Restangular, coachService) {
-  var coachId = $stateParams.coachId;
-
-  //获取单个教练数据
-  var baseCoaches = Restangular.one('coaches', coachId);
-  $scope.coach    = baseCoaches.get().$object;
-  $scope.courses  = baseCoaches.getList('courses').$object;
-  // $scope.comments = baseCoaches.getList('comments').$object;
-  
-
-  //初始化推荐教练列表
-  $scope.reCoaches = [];
-
-  var param = {
-    size: 4,
-    page: 0,
-    category_id: null,
-    gender: null
-  };
-
-  /**
-   * 查询推荐教练列表
-   * @param  {[obj]} param [page,size,category_id,gender]
-   * @return {[arr]}       [coachList]
-   */
-  coachService.allCoaches(param).then(function(res) {
-    console.log(res);
-    $scope.reCoaches = res;
-  }, function(err) {
-    console.log(err);
-  });
-
-  //课程安排modal
-  $ionicModal.fromTemplateUrl('app/components/modals/timetable/timetable.html', {
-    scope: $scope,
-    animation: 'slide-in-up',
-    hardwareBackButtonClose: true
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  //弹出课程安排modal
-  $scope.openSchedule = function() {
-    $scope.modal.show();
-  };
-
-  //关闭课程安排modal
-  $scope.closeSchedule = function() {
-    $scope.modal.hide();
-  };
-
-  //教练Tab
-  $scope.currentTab = "tab1";
-  $scope.setTab = function(tab) {
-    $scope.currentTab = tab;
-  };
-
-});
--- src/app/coach/coach.service.js
+++ src/app/coach/coach.service.js
@@ -2,33 +2,141 @@
 
 angular.module('1yd-coach')
 
-.service('coachService', function(Restangular) {
+  .service('coachService', function (Restangular) {
 
-  var coachPath = Restangular.all('coaches');
+    //var coachPath = Restangular.all('coaches');
+    //
+    ///**
+    // * 获取教练列表数据
+    // * @param  {[obj]} param [page,size,category_id,gender]
+    // * @return {[array]}       [coachList]
+    // */
+    //this.allCoaches = function (param) {
+    //  console.log(param);
+    //  return coachPath.getList(param);
+    //};
+    //
+    ///**
+    // * 获取单个教练数据
+    // * @param  {[number]} id [coachId]
+    // * @return {[obj]}    [coachData]
+    // */
+    //this.oneCoach = function (id) {
+    //  return coachPath.get(id);
+    //};
+    var base = Restangular.all('coaches')
+    Restangular.extendModel('coaches', function (elem) {
 
-  /**
-   * 获取教练列表数据
-   * @param  {[obj]} param [page,size,category_id,gender]
-   * @return {[array]}       [coachList]
-   */
-  this.allCoaches = function(param) {
-    console.log(param);
-    return coachPath.getList(param);
-  };
+      /**
+       * 获获取教练课程信息
+       * @param queryParams 查询参数
+       * @param headers header参数
+       * @returns {Object} Collection Promise
+       */
+      elem.courses = function (queryParams, headers) {
+        return this.getList('courses', queryParams, headers);
+      };
 
-  /**
-   * 获取单个教练数据
-   * @param  {[number]} id [coachId]
-   * @return {[obj]}    [coachData]
-   */
-  this.oneCoach = function(id) {
-    return coachPath.get(id);
-  };
-})
+      return elem;
+    });
 
-.service('meatDataService', function(Restangular) {
-  var meatDataPath = Restangular.one('meta-data');
-  this.meatData = function() {
-    return meatDataPath.get();
-  };
-});
+
+    /**
+     * 查询单个教练信息
+     * @param id
+     * @return {promise}
+     */
+    this.find = function (id) {
+      return base.get(id);
+    };
+
+    /**
+     * 刷新一个教练信息
+     * @param id
+     * @return {promise}
+     */
+    this.fresh = function (id) {
+      return base.get(id);
+    };
+
+    /**
+     * 保存教练信息
+     * @param data
+     * @param queryParams
+     * @param headers
+     * @return {promise}
+     */
+    this.save = function (data, queryParams, headers) {
+      return base.post(data, queryParams, headers);
+    };
+
+    /**
+     * 创建一个教练信息
+     * @param data
+     * @param queryParams
+     * @param headers
+     * @return {promise}
+     */
+    this.create = function (data, queryParams, headers) {
+      return base.post(data, queryParams, headers);
+    };
+
+  })
+
+  .service('meatDataService', function (Restangular) {
+    var meatDataPath = Restangular.one('meta-data');
+    this.meatData = function () {
+      return meatDataPath.get();
+    };
+  });
+
+
+angular.module('1yd-coach')
+  //.factory('Coach', function () {
+  //  function Book(bookData) {
+  //    if (bookData) {
+  //      this.setData(bookData);
+  //    }
+  //    // Some other initializations related to book
+  //  };
+  //  Book.prototype = {
+  //    setData: function (bookData) {
+  //      angular.extend(this, bookData);
+  //    },
+  //    load: function (id) {
+  //      var scope = this;
+  //      $http.get('ourserver/books/' + bookId).success(function (bookData) {
+  //        scope.setData(bookData);
+  //      });
+  //    },
+  //    delete: function () {
+  //      $http.delete('ourserver/books/' + bookId);
+  //    },
+  //    update: function () {
+  //      $http.put('ourserver/books/' + bookId, this);
+  //    },
+  //    getImageUrl: function (width, height) {
+  //      return 'our/image/service/' + this.book.id + '/' + width + '/' + height;
+  //    },
+  //    isAvailable: function () {
+  //      if (!this.book.stores || this.book.stores.length === 0) {
+  //        return false;
+  //      }
+  //      return this.book.stores.some(function (store) {
+  //        return store.quantity > 0;
+  //      });
+  //    }
+  //  };
+  //  return Book;
+  //})
+  .factory('CoachSvc', function (Restangular) {
+    Restangular.extendModel('coaches', function (elem) {
+
+      elem.courses = function (queryParams, headers) {
+        return this.getList('courses', queryParams, headers);
+      };
+
+      return elem;
+    });
+    return Restangular.all('coaches');
+  })
